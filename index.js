6e96029eba26b5d2f2f56f7458e2940ae775ef4b
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-04-14 20:31:22 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+
+const distanceFromHqInBlocks = (distinationLocation)=>{
+const headQuaters=42
+if (distinationLocation > headQuaters) {
+
+  return distinationLocation - headQuaters
+}else if (distinationLocation < headQuaters){
+    return headQuaters - distinationLocation
+
+ }
+}
+
+const distanceFromHqInFeet =(distinationLocation) =>{
+    const totalfeet=distanceFromHqInBlocks(distinationLocation) * 264
+    return totalfeet
+    
+}
+const distanceTravelledInFeet =(start, destination) =>{
+    
+    //function distanceTravelledInFeet(a,b);
+        if (start > destination) {
+           return ( start- destination) *264
+            }else if (destination > start) {
+            
+                return (destination- start) *264    
+            }
+        }
+            
+        const calculatesFarePrice = (start, destination) =>{
+            const distance=distanceTravelledInFeet(start, destination) 
+            if (distance  <= 400 ){ 
+                return (0)
+            }else if (distance > 400 && distance <2000){
+                return (distance -400)* .02
+
+             }else if (distance > 2000 && distance <2500) {
+               return (25)  
+             }else {
+                return 'cannot travel that far'
+             }
+
+            }
+        
